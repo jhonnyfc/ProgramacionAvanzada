06ba10706446a6c91d605f2327a73fb9baa7f5dc
Ruben <99rubenche@gmail.com>
2019-04-23 12:29:24 +0200

    ejemplos hilos y concurrencia

--- PA-P7-Concurrencia-Parte2/src/pa/paralelizacion/BarberoDurmienteRuben.java
+++ PA-P7-Concurrencia-Parte2/src/pa/paralelizacion/BarberoDurmienteRuben.java
@@ -1,6 +1,7 @@
 
 package pa.paralelizacion;
 
+import java.util.UUID;
 import java.util.concurrent.Semaphore;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -9,12 +10,12 @@ import java.util.logging.Logger;
  *
  * @author rabiixx on 23/04/2019
  */
-public class BarberoDurmienteRuben {
+public class BarberoDurmienteRuben extends Thread{
     
-    Semaphore barberoListo = new Semaphore(0);           // Mutex 0, 1
+    Semaphore barberoListo = new Semaphore(0);          // Mutex 0, 1
     Semaphore sillasAccesibles = new Semaphore(1);      // Si es 1, el numero de sillas puede ser modficado 
     Semaphore clientesEspera = new Semaphore(0);        // Numero de clientes sentados, listos para ser atendidos
-    int nSillasLibres = 5;                        // Numero de clientes 
+    int nSillasLibres = 5;                              // Numero de clientes 
     
     public class Barbero extends Thread{
         
@@ -39,7 +40,7 @@ public class BarberoDurmienteRuben {
         
         public void cortarPelo(){
             
-            System.out.println("[+] El barbero esta cortando el pelo");
+            System.out.println("[+] El barbero esta cortando el pelo ");
             try {
                 Thread.sleep(5000);
             } catch (InterruptedException ex) {
@@ -50,63 +51,74 @@ public class BarberoDurmienteRuben {
     }
     
     public class Cliente extends Thread {
+        
+        int id;
+        String name;
+
+        public Cliente(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
     
         public void run(){
             while(true){
                 try {
                     sillasAccesibles.acquire();
                     if (nSillasLibres > 0) {
+                        System.out.println("[+] " + this.name  + "-" + this.id + " se ha sentado");
                         nSillasLibres--;
                         clientesEspera.release();
                         sillasAccesibles.release();
                         barberoListo.acquire();
                         this.cortarPelo();
                     } else {
-                        
+                        System.out.println("[+] No hay sillas libres. Adios " + this.name + " - " + this.id);
+                        sillasAccesibles.release();
+                        break;
                     }
                 } catch (InterruptedException ex) {
                     Logger.getLogger(BarberoDurmienteRuben.class.getName()).log(Level.SEVERE, null, ex);
                 }
                 
+                
             }
         }
+        public void cortarPelo(){
+            
+            System.out.println("[+] El cliente " + id + " ha comenzado ha cortarse el pelo");
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(BarberoDurmienteRuben.class.getName()).log(Level.SEVERE, null, ex);
+            }       
+        }
 
+    }
     
+    public static void main(String[] args){
+        BarberoDurmienteRuben walid = new BarberoDurmienteRuben();
+        walid.start();
+    }
     
+    public void run(){
+        
+        Barbero barbero = new Barbero();
+        barbero.start();
+        
+        int i = 0;
+        String name = null;
+        
+        while (true) {
+            name = UUID.randomUUID().toString();
+            Cliente cliente = new Cliente(i, name);
+            cliente.start();
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(BarberoDurmienteRuben.class.getName()).log(Level.SEVERE, null, ex);
+            }  
+            ++i;
+        }
     }
     
 }
-/*
-def Customer():
-  while true:                   # Run in an infinite loop to simulate multiple customers.
-    wait(accessWRSeats)         # Try to get access to the waiting room chairs.
-    if numberOfFreeWRSeats > 0: # If there are any free seats:
-      numberOfFreeWRSeats -= 1  #   sit down in a chair
-      signal(custReady)         #   notify the barber, who's waiting until there is a customer
-      signal(accessWRSeats)     #   don't need to lock the chairs anymore
-      wait(barberReady)         #   wait until the barber is ready
-      # (Have hair cut here.)
-    else:                       # otherwise, there are no free seats; tough luck --
-      signal(accessWRSeats)     #   but don't forget to release the lock on the seats!
-      # (Leave without a haircut.)
-
-
-
-# The first two are mutexes (only 0 or 1 possible)
-Semaphore barberReady = 0
-Semaphore accessWRSeats = 1     # if 1, the number of seats in the waiting room can be incremented or decremented
-Semaphore custReady = 0         # the number of customers currently in the waiting room, ready to be served
-int numberOfFreeWRSeats = N     # total number of seats in the waiting room
-
-def Barber():
-  while true:                   # Run in an infinite loop.
-    wait(custReady)             # Try to acquire a customer - if none is available, go to sleep.
-    wait(accessWRSeats)         # Awake - try to get access to modify # of available seats, otherwise sleep.
-    numberOfFreeWRSeats += 1    # One waiting room chair becomes free.
-    signal(barberReady)         # I am ready to cut.
-    signal(accessWRSeats)       # Don't need the lock on the chairs anymore.
-    # (Cut hair here.)
-
-
-
-*/
